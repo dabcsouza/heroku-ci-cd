6f9b363f280ef92dc6e80814b74a6535248f2317
David Souza <dabcs@poli.ufrj.br>
2022-03-17 15:20:27 -0300

    configuração do app e criacao do Dockerfile

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => res.send(`<h2> Escutando na porta ${PORT}. </h2>`));
+app.listen(PORT, () => console.log(`Escutando na porta ${PORT}.`));
