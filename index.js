c46ad88b97f02b3fe127c3e171feed8762362704
David Souza <dabcs@poli.ufrj.br>
2022-03-17 15:46:27 -0300

    problemas de lint

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get('/', (_req, res) => res.send(`<h2> Escutando na porta ${PORT}. </h2>`));
-app.listen(PORT, () => console.log(`Escutando na porta ${PORT}.`));
+app.listen(PORT, () => console.log(`Escutando na porta: ${PORT}.`));
