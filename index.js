4bb802f8ad242a590617fcd604af347bae48290a
David Souza <dabcs@poli.ufrj.br>
2022-03-17 15:27:35 -0300

    manut

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.get('/', (_req, res) => res.send(`<h2> Escutando na porta ${PORT}. </h2>`));
