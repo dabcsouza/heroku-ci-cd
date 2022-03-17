0f866544c7fd48059669569b7ad1dc198d9d33c7
David Souza <dabcs@poli.ufrj.br>
2022-03-17 15:54:04 -0300

    alteracao de teste

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-app.get('/', (_req, res) => res.send(`<h2> Escutando na porta ${PORT}. </h2>`));
+app.get('/', (_req, res) => res.send(`<h2> Escutando na porta ${PORT}. Legal</h2>`));
 app.listen(PORT, () => console.log(`Escutando na porta: ${PORT}.`));
